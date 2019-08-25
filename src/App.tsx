f3e9dc6f9d729b689a5ab85b73e93f15fc70ab56
KobeRox <888888xc@gmail.com>
2019-08-26 02:16:07 +1200

    can read from db

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -9,36 +10,108 @@ const App: React.FC = () => {
   return (
     <Router>
       {/* <Route exact path="/" component={Home} /> */}
-      <Route path="/" component={Tweeter} />
+      <Route exact path="/" component={Tweeter} />
     </Router>
   );
 };
 
-class Tweeter extends React.Component<any, { title: string; post: string }> {
+interface IState {
+  title: "";
+  post: "";
+  tweets: object[];
+}
+
+class Tweeter extends React.Component<{}, IState> {
   constructor(props: any) {
     super(props);
     this.state = {
       title: "",
       post: "",
-      data
+      tweets: []
     };
   }
-  componentDidMount() {
-    fetch("https://tweeter.azurewebsites.net/index.html")
-      .then(response => response.json())
-      .then(response => this.setState({
-        text
-      }));
+
+  private getData = () => {
+    axios
+      .get(`https://tweeter.azurewebsites.net/api/Tweeters/`)
+      .then(response => {
+        console.log(response.data);
+        this.setState({ tweets: response.data });
+      })
+      .catch(error => {
+        // Error (copied catch error from https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253)
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  };
+
+  private postData = () => {
+    axios
+      .post("https://tweeter.azurewebsites.net/api/Tweeters/", {
+        title: "this is the post ",
+        post: ""
+      })
+      .then(response => {
+        console.log(response.data);
+        this.setState({ tweets: response.data });
+      });
+  };
+
+  private deleteData = () => {
+    axios
+      .delete("https://tweeter.azurewebsites.net/api/Tweeters/")
+      .then(response => {
+        console.log(response.data);
+        this.setState({ tweets: response.data });
+      });
+  };
+
+  public componentDidMount() {
+    this.getData();
   }
 
   handleSubmit(e: any) {
     e.preventDefault();
   }
-  render() {
+
+  public render() {
+    let { tweets } = this.state;
     return (
       <div>
         <h1>Tweeter</h1>
-        <form onSubmit={e => this.handleSubmit(e)} action="">
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input type="text" placeholder="Title" name="title" />
+          <br />
+          <br />
+          <textarea placeholder="Write a post " name="post"></textarea>
+          <br />
+          <br />
+          <button type="submit">Post</button>
+        </form>
+
+        <br></br>
+        <h2>Returned values</h2>
+        {tweets.map((item: any) => (
+          <div key={item.postid}>
+            <h1>{item.title}</h1>
+            <p>{item.post}</p>
+          </div>
+        ))}
+        {/* <form onSubmit={e => this.handleSubmit(e)} action="">
           <input
             type="text"
             placeholder="Title"
@@ -79,31 +152,10 @@ class Tweeter extends React.Component<any, { title: string; post: string }> {
               console.log(event);
             }}
           />
-        </form>
+        </form> */}
       </div>
     );
   }
 }
-
-const Home: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
 
 export default App;
